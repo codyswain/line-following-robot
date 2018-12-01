fa92dc0edd7374080169845e2b3248173b7ebc6c
Cody Swain <swain.cody@gmail.com>
2018-12-01 13:00:05 -0800

    Motor control attempt 1

--- motor_test/main.ino
+++ motor_test/main.ino
@@ -13,6 +13,59 @@ int sensorError[] = {-3, -2, -1, 0, 0, 1, 2, 3};
 int t_on = 12; //microseconds (us)
 int t_wait = 500000; //this value is really high?
 
+// Left Motor Pin Declarations
+const int LEFT_PWM = 40;
+const int LEFT_DIR = 14;
+const int LEFT_SLP = 31;
+
+// Right Motor Pin Declarations
+const int RIGHT_PWM = 39;
+const int RIGHT_DIR = 15;
+const int RIGHT_SLP = 11; 
+
+void setup(){
+  // Set up serial monitor
+  Serial.begin(9600);
+
+  // Set left motor pin modes
+  pinMode(LEFT_DIR, OUTPUT); 
+  pinMode(LEFT_PWM, OUTPUT); 
+  pinMode(LEFT_SLP, OUTPUT);
+
+  // Set right motor pin mode
+  pinMode(RIGHT_DIR, OUTPUT);
+  pinMode(RIGHT_PWM, OUTPUT);
+  pinMode(RIGHT_PWM, OUTPUT);
+
+  // LOW-Forward; HIGH-Reverse
+  // HIGH Control; LOW-Outputs floating (coast)
+  digitalWrite(LEFT_DIR, LOW);
+  digitalWrite(LEFT_SLP, HIGH);
+  digitalWrite(RIGHT_DIR, LOW);
+  digitalWrite(RIGHT_SLP, HIGH);
+}
+
+
+void loop(){
+  readSensors();
+
+  // Do something based off the sensor reading
+  float error = 0 ;
+  error = calculateMotorCorrection(); 
+  Serial.println(error);
+
+  correctMotors(error);
+}
+
+//void calculateProportionalError(){
+//  return 0;
+//}
+//
+//void calculateDerivativeError(){
+//  return 0;
+//}
+
+
 void readSensors(){
   /*
   How to read from sensors:
@@ -50,29 +103,6 @@ void readSensors(){
     delay(1);
 }
 
-void setup(){
-  // Set up serial monitor
-  Serial.begin(9600);
-}
-
-void loop(){
-  readSensors();
-
-  // Do something based off the sensor reading
-  float error = 0 ;
-  error = calculateMotorCorrection(); 
-  Serial.println(error);
-
-}
-
-//void calculateProportionalError(){
-//  return 0;
-//}
-//
-//void calculateDerivativeError(){
-//  return 0;
-//}
-
 float calculateMotorCorrection(){
   float error = 0;
   float sum = 0;
@@ -85,6 +115,25 @@ float calculateMotorCorrection(){
   }
   error = sum / activePinCount;
   return error; 
+}
+
+void correctMotors(float error){
+	if (error == 0){
+		digitalWrite(LEFT_DIR, LOW);
+		digitalWrite(RIGHT_DIR, LOW);
+		analogWrite(LEFT_PWM, 60);
+		analogWrite(RIGHT_PWM, 60);
+	} else if (error > 0){
+		digitalWrite(LEFT_DIR, LOW);
+		digitalWrite(RIGHT_DIR, LOW);
+		analogWrite(LEFT_PWM, 60);
+		analogWrite(RIGHT_PWM, 0);
+	} else if (error < 0){
+		digitalWrite(LEFT_DIR, LOW);
+		digitalWrite(RIGHT_DIR, LOW);
+		analogWrite(LEFT_PWM, 0);
+		analogWrite(RIGHT_PWM, 60);
+	}
 }
 
 
